93f5b647ee2ffcf33278db5ebace8d9605cf7758
CiwAI Smart Digital <ciwaismartdigital@gmail.com>
2025-09-02 01:17:32 +0700

    Fix: switch to env.env for flutter_dotenv compatibility

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Load environment variables
-await dotenv.load(fileName: "env.json");
+await dotenv.load(fileName: "env.env");
   runApp(const CiwaiDriveApp());
 
   print("ENV Loaded: ${dotenv.env}");
